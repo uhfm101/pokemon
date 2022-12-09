3555889c96f962c9e01c73e9ca0e6180635cf40d
uhfm <uhfm@fresnou.org>
2022-12-09 11:10:04 -0800

    modules

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,33 +1,5 @@
-module.exports.viewAll = function(req, res, next){
-    const pokemons = [{
-        id: 1,
-        name: 'Mudkip',
-        type: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg/1024px-Pok%C3%A9mon_Water_Type_Icon.svg.png',
-        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png',
-        health: 50,
-        attack1: 'Water Gun',
-        damage1: 40,
-        energy1: 1,
-        attack2: 'Water Pulse',
-        damage2: 60,
-        energy2: 2,
-        weakness: 1,
-        resistance: 1,
-        retreatC: 1,
-    }, {  id: 2,
-        name: 'Mudkip',
-        type: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg/1024px-Pok%C3%A9mon_Water_Type_Icon.svg.png',
-        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png',
-        health: 50,
-        attack1: 'Water Gun',
-        damage1: 40,
-        energy1: 1,
-        attack2: 'Water Pulse',
-        damage2: 60,
-        energy2: 2,
-        weakness: 1,
-        resistance: 1,
-        retreatC: 1,
-}]
+const {Pokemon} = require('../models')
+module.exports.viewAll = async function(req, res, next){
+    const pokemons = await Pokemon.findAll();
     res.render('index', {pokemons})
 }
