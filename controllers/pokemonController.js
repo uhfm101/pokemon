790e5f5ce3ae9d3aa18a471c69efd4b9a985df38
uhfm <uhfm@fresnou.org>
2022-12-12 11:37:25 -0800

    Forms

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,5 +1,13 @@
 const {Pokemon} = require('../models')
+const types = ['Water', 'Fire', 'Grass', 'Rock', 'Psychic']
 module.exports.viewAll = async function(req, res, next){
     const pokemons = await Pokemon.findAll();
     res.render('index', {pokemons})
+}
+
+module.exports.renderEditForm = async function(req, res, next){
+    const pokemon = await Pokemon.findByPk(
+        req.params.id
+    );
+    res.render('edit', {pokemon});
 }
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,6 @@ const pokemonController = require('../controllers/pokemonController')
 
 router.get('/', pokemonController.viewAll)
 
+router.get('/edit/:id', pokemonController.renderEditForm)
+
 module.exports = router;
