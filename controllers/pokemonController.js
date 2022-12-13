b1dd10a8780f7087d6becf27a9f183fdd03202be
uhfm <uhfm@fresnou.org>
2022-12-13 13:23:48 -0800

    Forms

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -9,5 +9,81 @@ module.exports.renderEditForm = async function(req, res, next){
     const pokemon = await Pokemon.findByPk(
         req.params.id
     );
-    res.render('edit', {pokemon});
+    res.render('edit', {pokemon, types});
+}
+
+module.exports.updateCard = async function(req, res){
+    await Pokemon.update(
+        {
+            name: req.body.name,
+            type: req.body.type,
+            image: req.body.image,
+            health: req.body.health,
+            attack1: req.body.attack1,
+            damage1: req.body.damage1,
+            energy1: req.body.energy1,
+            attack2: req.body.attack2,
+            damage2: req.body.damage2,
+            energy2: req.body.energy2,
+            weakness: req.body.weakness,
+            resistance: req.body.resistance,
+            retreatC: req.body.retreatC
+        },
+               {
+            where:
+                {
+                    id: req.params.id
+                }
+        });
+    res.redirect('/')
+}
+
+module.exports.deleteCard = async function(req, res){
+    await Pokemon.destroy(
+        {
+            where:
+                {
+                    id: req.params.id
+                }
+        })
+    res.redirect('/')
+}
+
+module.exports.renderAddForm = function(req, res){
+    const pokemon = {
+        name: " ",
+        type: types[0],
+        image: " ",
+        health: 1,
+        attack1: " ",
+        damage1: 1,
+        energy1: 1,
+        attack2: " ",
+        damage2: 1,
+        energy2: 1,
+        weakness: 1,
+        resistance: 1,
+        retreatC: 1
+    };
+    res.render('add', {pokemon, types});
+}
+
+module.exports.addCard = async function(req, res){
+    await Pokemon.create(
+        {
+            name: req.body.name,
+            type: req.body.type,
+            image: req.body.image,
+            health: req.body.health,
+            attack1: req.body.attack1,
+            damage1: req.body.damage1,
+            energy1: req.body.energy1,
+            attack2: req.body.attack2,
+            damage2: req.body.damage2,
+            energy2: req.body.energy2,
+            weakness: req.body.weakness,
+            resistance: req.body.resistance,
+            retreatC: req.body.retreatC
+        });
+    res.redirect('/')
 }
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get('/', pokemonController.viewAll)
 
 router.get('/edit/:id', pokemonController.renderEditForm)
 
+router.post('/edit/:id', pokemonController.updateCard)
+
+router.get('/delete/:id', pokemonController.deleteCard)
+
+router.get('/add', pokemonController.renderAddForm)
+
 module.exports = router;
