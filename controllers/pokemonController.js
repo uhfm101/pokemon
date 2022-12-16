5a945798a6d4bed54ab4c22dc3fe7764caf16f3a
uhfm <uhfm@fresnou.org>
2022-12-16 11:29:45 -0800

    code

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,5 +1,6 @@
 const {Pokemon} = require('../models')
-const types = ['Water', 'Fire', 'Grass', 'Rock', 'Psychic']
+const e = require("express");
+const types = ['Water', 'Fire', 'Grass', 'Rock', 'Electric']
 module.exports.viewAll = async function(req, res, next){
     const pokemons = await Pokemon.findAll();
     res.render('index', {pokemons})
@@ -27,7 +28,7 @@ module.exports.updateCard = async function(req, res){
             energy2: req.body.energy2,
             weakness: req.body.weakness,
             resistance: req.body.resistance,
-            retreatC: req.body.retreatC
+            retreatC: req.body.retreatC,
         },
                {
             where:
@@ -69,25 +70,38 @@ module.exports.renderAddForm = function(req, res){
     res.render('add', {pokemon, types});
 }
 
-module.exports.addCard = function(req, res){
-    console.log("run")
-    console.log(req.body.name)
-   //  const newP = await Pokemon.create(
-   //      {
-   //          name: req.body.name,
-   //          type: req.body.type,
-   //          image: req.body.image,
-   //          health: req.body.health,
-   //          attack1: req.body.attack1,
-   //          damage1: req.body.damage1,
-   //          energy1: req.body.energy1,
-   //          attack2: req.body.attack2,
-   //          damage2: req.body.damage2,
-   //          energy2: req.body.energy2,
-   //          weakness: req.body.weakness,
-   //          resistance: req.body.resistance,
-   //          retreatC: req.body.retreatC
-   //      });
-   // console.log(newP)
+module.exports.addCard = async function(req, res){
+    await Pokemon.create(
+        {
+            name: req.body.name,
+            type: req.body.type,
+            image: req.body.image,
+            health: req.body.health,
+            attack1: req.body.attack1,
+            damage1: req.body.damage1,
+            energy1: req.body.energy1,
+            attack2: req.body.attack2,
+            damage2: req.body.damage2,
+            energy2: req.body.energy2,
+            weakness: req.body.weakness,
+            resistance: req.body.resistance,
+            retreatC: req.body.retreatC,
+        });
     res.redirect('/')
+}
+
+function getType(type){
+    if (type === "Water"){
+        return "water"
+    } else if (type === "Fire"){
+        return "fire"
+    } else if (type === "Grass"){
+        return "grass"
+    } else if (type === "Rock"){
+        return "rock"
+    } else if (type === "Electric"){
+        return "electric"
+    } else{
+        return ""
+    }
 }
